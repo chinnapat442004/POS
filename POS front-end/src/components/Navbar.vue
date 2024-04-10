615213234aa783f09a53bd4b65ac5aa94a30faf1
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-04-10 16:40:05 +0700

    Add Navigation drawers

---
+++ POS front-end/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const drawer = ref(true)
+const rail = ref(true)
+</script>
+
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          title="John Leider"
+          nav
+        >
+          <template v-slot:append>
+            <v-btn icon="mdi-chevron-left" variant="text" @click.stop="rail = !rail"></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item
+            prepend-icon="mdi-coffee"
+            title="Point of Sale"
+            value="pos"
+            :to="{ name: 'pos' }"
+          ></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-account-group-outline"
+            title="About"
+            value="about"
+            :to="{ name: 'about' }"
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
