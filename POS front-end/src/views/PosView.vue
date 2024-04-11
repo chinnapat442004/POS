791af06ba5b30885efe82c16c19f38e60532a910
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-04-11 16:47:43 +0700

    Add http service

--- POS front-end/src/views/PosView.vue
+++ POS front-end/src/views/PosView.vue
@@ -1,15 +1,26 @@
-<template>
-  <div class="about">
-    <h1>This is an pos page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+const tab = ref(null)
+</script>
+<template>
+  <v-container>
+    <v-card width="500px">
+      <v-tabs v-model="tab" bg-color="primary">
+        <v-tab value="one">Drink</v-tab>
+        <v-tab value="two">Item Two</v-tab>
+        <v-tab value="three">Item Three</v-tab>
+      </v-tabs>
+
+      <v-card-text>
+        <v-window v-model="tab">
+          <v-window-item value="one"> One </v-window-item>
+
+          <v-window-item value="two"> Two </v-window-item>
+
+          <v-window-item value="three"> Three </v-window-item>
+        </v-window>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
