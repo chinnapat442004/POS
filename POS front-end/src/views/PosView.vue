cb556d5d69579afa475a89b119bf09eeb478fa41
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-04-13 12:35:27 +0700

    Add category filter for products in PosView

--- POS front-end/src/views/PosView.vue
+++ POS front-end/src/views/PosView.vue
@@ -3,25 +3,32 @@ import { useProductStore } from '@/stores/product'
 import { onMounted, ref } from 'vue'
 const productStore = useProductStore()
 
-onMounted(() => {
-  productStore.getPorducts()
+onMounted(async () => {
+  await productStore.getPorducts()
 })
+
 const tab = ref(null)
 </script>
 <template>
   <v-container>
-    <v-card width="600px">
-      <v-tabs v-model="tab" bg-color="primary">
-        <v-tab value="one">Drink</v-tab>
-        <v-tab value="two">Item Two</v-tab>
-        <v-tab value="three">Item Three</v-tab>
+    <v-card max-width="600px">
+      <v-tabs v-model="tab" style="background-color: rgb(50, 50, 50); color: white">
+        <v-tab value="one">Drinks</v-tab>
+        <v-tab value="two">Foods</v-tab>
+        <v-tab value="three">Desserts</v-tab>
       </v-tabs>
 
-      <v-window v-model="tab">
+      <v-window v-model="tab" style="background-color: rgb(180, 180, 180)">
         <v-window-item value="one">
           <v-container>
             <v-row>
-              <v-col v-for="item of productStore.products" :key="item.id" md="4">
+              <v-col
+                v-for="item of productStore.products.filter(
+                  (product) => product.category === 'drink'
+                )"
+                :key="item.id"
+                md="4"
+              >
                 <v-card elevation="2" width="160" @click=""
                   ><v-img
                     :src="`http://localhost:3000/images/products/${item.image}`"
@@ -34,9 +41,49 @@ const tab = ref(null)
           >
         </v-window-item>
 
-        <v-window-item value="two"> Two </v-window-item>
+        <v-window-item value="two">
+          <v-container>
+            <v-row>
+              <v-col
+                v-for="item of productStore.products.filter(
+                  (product) => product.category === 'food'
+                )"
+                :key="item.id"
+                md="4"
+              >
+                <v-card elevation="2" width="160" @click=""
+                  ><v-img
+                    :src="`http://localhost:3000/images/products/${item.image}`"
+                    height="150"
+                  ></v-img>
+                  {{ item.name }}</v-card
+                >
+              </v-col>
+            </v-row></v-container
+          ></v-window-item
+        >
 
-        <v-window-item value="three"> Three </v-window-item>
+        <v-window-item value="three">
+          <v-container>
+            <v-row>
+              <v-col
+                v-for="item of productStore.products.filter(
+                  (product) => product.category === 'dessert'
+                )"
+                :key="item.id"
+                md="4"
+              >
+                <v-card elevation="2" width="160" @click=""
+                  ><v-img
+                    :src="`http://localhost:3000/images/products/${item.image}`"
+                    height="150"
+                  ></v-img>
+                  {{ item.name }}</v-card
+                >
+              </v-col>
+            </v-row></v-container
+          ></v-window-item
+        >
       </v-window>
     </v-card>
   </v-container>
