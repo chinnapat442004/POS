984bdc4d963b775acace407ca97ff2e7527d431a
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-04-15 10:45:57 +0700

    Add initialProcuct and procuct in procuct stores

--- POS front-end/src/stores/product.ts
+++ POS front-end/src/stores/product.ts
@@ -1,4 +1,4 @@
-import type { Product } from '@/types/Procuct'
+import type { Product } from '@/types/Product'
 import { defineStore } from 'pinia'
 import porductService from '@/service/product'
 import { ref } from 'vue'
@@ -6,12 +6,20 @@ import { ref } from 'vue'
 export const useProductStore = defineStore('product', () => {
   const products = ref<Product[]>([])
 
+  const initialProduct: Product = {
+    name: '',
+    price: 0
+  }
+
+  const product = ref(<Product>JSON.parse(JSON.stringify(initialProduct)))
+
   async function getPorducts() {
     const res = await porductService.getProduct()
     products.value = res.data
   }
   return {
     getPorducts,
-    products
+    products,
+    product
   }
 })
