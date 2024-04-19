6f8ec062aaacb96409deffdcbb34efd6bcd53ee9
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-04-19 11:57:21 +0700

    Imported main.css file into main.ts

--- POS front-end/src/main.ts
+++ POS front-end/src/main.ts
@@ -3,8 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
 
-// Vuetify
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
@@ -13,7 +13,7 @@ import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
   icons: {
-    defaultSet: 'mdi' // This is already the default value - only for display purposes
+    defaultSet: 'mdi'
   },
   components,
   directives
