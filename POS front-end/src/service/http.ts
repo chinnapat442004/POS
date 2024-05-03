ea727ef54f6df6ca386a3587f56752119e55d011
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-05-03 15:09:10 +0700

    Add authorization header to Axios requests

--- POS front-end/src/service/http.ts
+++ POS front-end/src/service/http.ts
@@ -4,4 +4,17 @@ const instance = axios.create({
   baseURL: 'http://localhost:3000'
 })
 
+instance.interceptors.request.use(
+  async function (config) {
+    const token = localStorage.getItem('access_token')
+    if (token) {
+      config.headers.Authorization = 'Bearer ' + token
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
 export default instance
