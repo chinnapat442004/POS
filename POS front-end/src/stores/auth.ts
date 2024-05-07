6959041054c4d91186538cc0492fa2d0cf2fa633
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-05-07 11:00:43 +0700

    Add getCurrentEmployee and  clearCurrentEmployee function in auth store

--- POS front-end/src/stores/auth.ts
+++ POS front-end/src/stores/auth.ts
@@ -2,25 +2,45 @@ import { defineStore } from 'pinia'
 import authService from '@/service/aunth'
 import { ref } from 'vue'
 import type { Employee } from '@/types/Employee'
+import router from '@/router'
 
 export const useAuthStore = defineStore('auth', () => {
-  const email = ref('employee1@mail.com')
-  const password = ref('Pass@1234')
-  const currentEmployee = ref<Employee>()
+  const email = ref('')
+  const password = ref('')
+  const currentEmployee = ref<Employee | null>()
 
   async function login() {
     const res = await authService.auth(email.value, password.value)
-    localStorage.setItem('employee', JSON.stringify(res.data.employee))
-    localStorage.setItem('access_token', res.data.access_token)
-  }
-
-  async function getCurrentEmployee() {
-    const employeeData = localStorage.getItem('employee')
-    if (employeeData != null) {
-      currentEmployee.value = await JSON.parse(employeeData)
+    if (res) {
+      res.data.access_token
+      localStorage.setItem('employee', JSON.stringify(res.data.employee))
+      localStorage.setItem('access_token', res.data.access_token)
+      await router.replace({ name: 'pos' })
     }
-
-    console.log(currentEmployee.value?.name)
   }
-  return { login, getCurrentEmployee, currentEmployee }
+
+  // async function getCurrentEmployee(): Employee | null {
+  //   const employeeData = localStorage.getItem('employee')
+  //   if (employeeData != null) {
+  //     currentEmployee.value = await JSON.parse(employeeData)
+  //     if (currentEmployee.value) return currentEmployee.value
+  //   }
+  //   return null
+  // }
+
+  async function getCurrentEmployee(): Promise<Employee | null> {
+    const employeeData = localStorage.getItem('employee')
+    if (employeeData) {
+      currentEmployee.value = await JSON.parse(employeeData)
+      if (currentEmployee.value) return currentEmployee.value
+    }
+    return null
+  }
+
+  function clearCurrentEmployee() {
+    localStorage.setItem('employee', '')
+    localStorage.setItem('access_token', '')
+    router.replace({ name: 'login' })
+  }
+  return { login, getCurrentEmployee, clearCurrentEmployee, currentEmployee, email, password }
 })
