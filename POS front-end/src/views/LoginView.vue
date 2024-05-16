660798892733a3278b4ac27c24923d259c3eb6c3
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-05-16 11:38:29 +0700

    Add MemberView

--- POS front-end/src/views/LoginView.vue
+++ POS front-end/src/views/LoginView.vue
@@ -37,67 +37,71 @@ onMounted(async () => {
 // const pass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
 </script>
 <template>
-  <v-card
-    style="margin: auto; margin-top: 80px; border-radius: 10px; background-color: #4f5d75"
-    height="500"
-    width="600"
-  >
-    <v-form ref="form" @submit.prevent="onSubmit">
-      <div style="text-align: center; font-size: 30px; color: white">Login</div>
-      <div style="text-align: center">
-        <v-avatar size="180">
-          <v-img src="/src/images/noimage.jpg" style="margin: auto"></v-img>
-        </v-avatar>
-      </div>
-
-      <v-container>
-        <div style="font-size: 17px; color: white">Email</div>
-        <v-text-field
-          class="style"
-          :readonly="loading"
-          v-model="authStore.email"
-          density="compact"
-          placeholder="Email address"
-          prepend-inner-icon="mdi-email-outline"
-          bg-color="blue-grey-lighten-5"
-          variant="outlined"
-          :rules="[(email) => !!email.match(emailPattern) || 'Invalid email address'] || loginRules"
-        ></v-text-field>
-        <div style="font-size: 17px; color: white" class="align-center justify-space-between">
-          Password
+  <div style="height: 627px">
+    <v-card
+      style="margin: auto; margin-top: 80px; border-radius: 10px; background-color: #4f5d75"
+      height="500"
+      width="600"
+    >
+      <v-form ref="form" @submit.prevent="onSubmit">
+        <div style="text-align: center; font-size: 30px; color: white">Login</div>
+        <div style="text-align: center">
+          <v-avatar size="180">
+            <v-img src="/src/images/noimage.jpg" style="margin: auto"></v-img>
+          </v-avatar>
         </div>
-        <v-text-field
-          class="style"
-          :rules="[loginRules]"
-          v-model="authStore.password"
-          :readonly="loading"
-          :append-inner-icon="show ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="show ? 'text' : 'password'"
-          density="compact"
-          bg-color="blue-grey-lighten-5"
-          placeholder="Enter your password"
-          prepend-inner-icon="mdi-lock-outline"
-          variant="outlined"
-          @click:append-inner="show = !show"
-        ></v-text-field>
 
-        <br />
+        <v-container>
+          <div style="font-size: 17px; color: white">Email</div>
+          <v-text-field
+            class="style"
+            :readonly="loading"
+            v-model="authStore.email"
+            density="compact"
+            placeholder="Email address"
+            prepend-inner-icon="mdi-email-outline"
+            bg-color="blue-grey-lighten-5"
+            variant="outlined"
+            :rules="
+              [(email) => !!email.match(emailPattern) || 'Invalid email address'] || loginRules
+            "
+          ></v-text-field>
+          <div style="font-size: 17px; color: white" class="align-center justify-space-between">
+            Password
+          </div>
+          <v-text-field
+            class="style"
+            :rules="[loginRules]"
+            v-model="authStore.password"
+            :readonly="loading"
+            :append-inner-icon="show ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="show ? 'text' : 'password'"
+            density="compact"
+            bg-color="blue-grey-lighten-5"
+            placeholder="Enter your password"
+            prepend-inner-icon="mdi-lock-outline"
+            variant="outlined"
+            @click:append-inner="show = !show"
+          ></v-text-field>
 
-        <v-btn
-          @click="login"
-          :loading="loading"
-          class="style"
-          size="large"
-          type="submit"
-          variant="elevated"
-          block
-          style="background-color: #0d1b2a; color: white"
-        >
-          Login
-        </v-btn>
-      </v-container>
-    </v-form>
-  </v-card>
+          <br />
+
+          <v-btn
+            @click="login"
+            :loading="loading"
+            class="style"
+            size="large"
+            type="submit"
+            variant="elevated"
+            block
+            style="background-color: #0d1b2a; color: white"
+          >
+            Login
+          </v-btn>
+        </v-container>
+      </v-form>
+    </v-card>
+  </div>
 </template>
 <style>
 .style {
