fe4e437295d2201edec440b9a08ba3eb5f14fe8c
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-05-16 11:13:37 +0700

    Added getReceipts, getReceipt, formatDate, and createReceipt functions to the receipt store.

--- POS front-end/src/stores/receipt.ts
+++ POS front-end/src/stores/receipt.ts
@@ -1,6 +1,9 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { useReceiptItemStore } from './receiptItem'
+import receiptService from '@/service/receipt'
+import type { Receipt } from '@/types/Receipt'
+import { format } from 'date-fns'
 
 export const useReceiptStore = defineStore('receipt', () => {
   const receiptItemStore = useReceiptItemStore()
@@ -12,11 +15,40 @@ export const useReceiptStore = defineStore('receipt', () => {
 
   const receipt = ref(<Receipt>JSON.parse(JSON.stringify(initialRecipt)))
 
+  async function getReceipts() {
+    const res = await receiptService.getReceipts()
+
+    receipts.value = res.data
+    receipts.value.forEach((item) => {
+      if (item.created && typeof item.created === 'string') {
+        const date = new Date(item.created)
+        item.created = format(date, 'dd/MM/yyyy HH:mm:ss')
+      }
+    })
+  }
+
+  async function getReceipt(item: Receipt) {
+    const res = await receiptService.getReceipt(item)
+    receipt.value = res.data
+    formatDate()
+  }
+
   function calTotal() {
     clearReceipt()
     receiptItemStore.receiptItems.forEach((item) => {
       receipt.value.total += item.total
     })
+  }
+
+  async function formatDate() {
+    const date = new Date()
+    const formattedDate: string = format(date, 'dd/MM/yyyy HH:mm')
+    receipt.value.created = await formattedDate
+  }
+
+  function createReceipt() {
+    formatDate()
+    receiptService.createReceipt(receipt.value, receiptItemStore.receiptItems)
   }
 
   function clearReceipt() {
@@ -26,6 +58,9 @@ export const useReceiptStore = defineStore('receipt', () => {
   return {
     receipts,
     receipt,
-    calTotal
+    calTotal,
+    createReceipt,
+    getReceipts,
+    getReceipt
   }
 })
