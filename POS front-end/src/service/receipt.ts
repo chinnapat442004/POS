ad8b36d6a38f651cb0e2e12bee94ab3c9a1c0458
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-05-16 11:10:37 +0700

    Fix Receipt type and add receipt service

---
+++ POS front-end/src/service/receipt.ts
@@ -0,0 +1,38 @@
+import type { ReceiptItme } from '@/types/ReceiptItem'
+import http from './http'
+import type { Receipt } from '@/types/Receipt'
+import type { Product } from '@/types/Product'
+import type { Employee } from '@/types/Employee'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+type ReceiptDto = {
+  receiptItems: {
+    quantity: number
+    total: number
+    product: Product
+  }[]
+  employee?: Employee
+}
+
+async function createReceipt(receipt: Receipt, receiptItems: ReceiptItme[]) {
+  const receiptDto: ReceiptDto = {
+    receiptItems: []
+  }
+  receiptDto.receiptItems = receiptItems.map((item) => {
+    return { product: item.product, quantity: item.quantity, total: item.total }
+  })
+  receiptDto.employee = await authStore.currentEmployee
+
+  return http.post('/receipts', receiptDto)
+}
+
+function getReceipts() {
+  return http.get('/receipts')
+}
+
+function getReceipt(receipt: Receipt) {
+  return http.get(`/receipts/${receipt.id}`)
+}
+
+export default { createReceipt, getReceipts, getReceipt }
--- POS front-end/src/types/Receipt.ts
+++ POS front-end/src/types/Receipt.ts
@@ -1,4 +1,12 @@
+import type { Employee } from './Employee'
+
+import type { ReceiptItme } from './ReceiptItem'
+
 type Receipt = {
   id: number
   total: number
+  receiptItems?: ReceiptItme[]
+  created?: string
+  employee?: Employee
 }
+export { type Receipt }
