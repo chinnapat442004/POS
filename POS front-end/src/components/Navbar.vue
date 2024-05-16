c1834b6c0ef5fbb2f56c92955c9a293dda7fa7ee
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-05-16 11:38:29 +0700

    Add MemberView

--- POS front-end/src/components/Navbar.vue
+++ POS front-end/src/components/Navbar.vue
@@ -12,7 +12,6 @@ import conformLogoutDialog from '@/views/ConfirmLogoutDialog.vue'
 provide('logoutDialog', dialog)
 
 onMounted(async () => {
-  console.log(localStorage.getItem('employee'))
   await authStore.getCurrentEmployee()
 })
 
@@ -29,12 +28,48 @@ function confirmLogout() {
     @click="rail = false"
     style="background-color: #4f5d75"
   >
-    <v-list-item
-      style="background-color: #0d1b2a; color: white"
-      :prepend-avatar="`http://localhost:3000/images/employees/${authStore.currentEmployee?.image}`"
-      nav
-    >
-      <h4 style="font-size: 20px; font-weight: bold">{{ authStore.currentEmployee?.name }}</h4>
+    <v-list-item style="background-color: #0d1b2a; height: 80px; color: white" nav>
+      <v-menu min-width="200px" rounded>
+        <template v-slot:activator="{ props }">
+          <div>
+            <div style="display: inline-block; margin-left: 10px">
+              <h4 style="font-size: 20px; font-weight: bold">
+                {{ authStore.currentEmployee?.name }}
+              </h4>
+            </div>
+
+            <div style="display: inline-block; margin-left: 25px">
+              <v-btn icon v-bind="props" style="width: 100%; height: 100%">
+                <v-avatar>
+                  <v-img
+                    :src="`http://localhost:3000/images/employees/${authStore.currentEmployee?.image}`"
+                  ></v-img
+                ></v-avatar>
+              </v-btn>
+            </div>
+          </div>
+        </template>
+        <v-card>
+          <v-card-text>
+            <div class="mx-auto text-center">
+              <v-avatar>
+                <v-img
+                  :src="`http://localhost:3000/images/employees/${authStore.currentEmployee?.image}`"
+                ></v-img>
+              </v-avatar>
+              <h3>{{ authStore.currentEmployee?.name }}</h3>
+              <p class="text-caption mt-1">
+                {{ authStore.currentEmployee?.email }}
+              </p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn variant="text" rounded> Edit Account </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn variant="text" rounded> Disconnect </v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-menu>
+
       <template v-slot:append>
         <v-btn icon="mdi-chevron-left" variant="text" @click.stop="rail = !rail"></v-btn>
       </template>
@@ -50,23 +85,64 @@ function confirmLogout() {
         value="pos"
         :to="{ name: 'pos' }"
       ></v-list-item>
-      <v-list-item
-        class="list"
-        prepend-icon="mdi-food"
-        title="Procudt"
-        value="product"
-        :to="{ name: 'product' }"
-      ></v-list-item>
 
       <v-list-item
         class="list"
-        prepend-icon="mdi-account-group"
-        title="Employee"
-        value="employee"
-        :to="{ name: 'employee' }"
+        prepend-icon="mdi-history"
+        title="History"
+        value="history"
+        :to="{ name: 'history' }"
       ></v-list-item>
-      <div style="height: 425px"></div>
       <v-list-item
+        class="list"
+        prepend-icon="mdi-clock-check-outline"
+        title="Check In Check Out"
+        value="check-in"
+        :to="{ name: 'check-in' }"
+      ></v-list-item>
+
+      <v-list-group value="management">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            title="Management"
+            class="list"
+            prepend-icon="mdi-pencil"
+          ></v-list-item>
+        </template>
+        <v-list-item
+          class="list"
+          prepend-icon="mdi-food"
+          title="Procudt"
+          value="product"
+          :to="{ name: 'product' }"
+        ></v-list-item>
+        <v-list-item
+          class="list"
+          prepend-icon="mdi-account-group"
+          title="Employee"
+          value="employee"
+          :to="{ name: 'employee' }"
+        ></v-list-item>
+        <v-list-item
+          class="list"
+          prepend-icon="mdi-account"
+          title="Member"
+          value="member"
+          :to="{ name: 'member' }"
+        ></v-list-item>
+      </v-list-group>
+
+      <v-list-item
+        class="list"
+        prepend-icon="mdi-chart-bar"
+        title="Chart"
+        value="chart"
+        :to="{ name: 'chart' }"
+      ></v-list-item>
+
+      <v-list-item
+        style="position: fixed; bottom: 2px; width: 95%"
         class="list"
         prepend-icon="mdi-logout"
         title="Logout"
---
+++ POS front-end/src/views/MemberView.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+import { useMemberStore } from '@/stores/member'
+
+const memberStore = useMemberStore()
+const search = ref('')
+const deleteDialog = ref(false)
+
+onMounted(async () => {
+  await memberStore.getMembers()
+})
+
+const headers = [
+  { title: 'Id', value: 'id', key: 'id' },
+  { title: 'Name', value: 'name', key: 'name' },
+  { title: 'Phone', value: 'phone', key: 'phone' },
+  { title: 'Point', value: 'point', key: 'point' },
+  { title: 'Actions', key: 'actions', sortable: false }
+]
+</script>
+<template>
+  <v-card elevation="5" height="648" style="margin: 30px 50px">
+    <v-row>
+      <v-data-table
+        height="530"
+        :headers="headers"
+        :items="memberStore.members"
+        :search="search"
+        :header-props="{ style: 'background-color: #e1e5f2;  font-weight: 800; ' }"
+      >
+        <template v-slot:top>
+          <v-toolbar flat style="background-color: #0d1b2a; color: white; margin-top: 12px">
+            <v-toolbar-title style="background-color: #0d1b2a; color: white">
+              <v-row
+                ><v-col md="6">
+                  <div style="margin: 10px 0px 0px 10px; font-size: 25px">Member</div>
+                </v-col>
+                <v-col md="4">
+                  <v-text-field
+                    style="background: white; color: #0d1b2a; border-radius: 15px"
+                    v-model="search"
+                    label="Search"
+                    prepend-inner-icon="mdi-magnify"
+                    variant="outlined"
+                    hide-details
+                    single-line
+                    color="white"
+                  ></v-text-field
+                ></v-col>
+                <v-col md="2">
+                  <div style="margin: 10px 10px 0px 0px">
+                    <edited-employee-dialog></edited-employee-dialog>
+                  </div> </v-col></v-row
+            ></v-toolbar-title>
+          </v-toolbar>
+        </template>
+
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon class="me-2" size="small" @click="open(item)" color="#263238">
+            mdi-pencil
+          </v-icon>
+          <v-icon size="small" @click="deleteItem(item)" color="#E57373"> mdi-delete </v-icon>
+        </template>
+      </v-data-table>
+    </v-row>
+  </v-card>
+
+  <delete-employee-dialog></delete-employee-dialog>
+</template>
