94cbdeacac9e92e31212994e2fb72c4005f7a6cd
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-05-18 16:54:21 +0700

    Add member service

--- POS front-end/src/service/member.ts
+++ POS front-end/src/service/member.ts
@@ -1,7 +1,20 @@
+import type { Member } from '@/types/Member'
 import http from './http'
 
 function getMembers() {
   return http.get('/members')
 }
 
-export default { getMembers }
+function getMemberByPhone(phone: string) {
+  return http.get(`members/phone/${phone}`)
+}
+
+function deleteMember(member: Member) {
+  return http.delete(`members/${member.id}`)
+}
+
+function getMember(member: Member) {
+  return http.get(`members/${member.id}`)
+}
+
+export default { getMembers, getMemberByPhone, deleteMember, getMember }
