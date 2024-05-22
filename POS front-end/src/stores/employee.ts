46b8b208613b385db3ae7be5d2cdc1fe9c0b1fc7
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-05-22 10:53:27 +0700

    Add etEmployeeByBranch in employee

--- POS front-end/src/stores/employee.ts
+++ POS front-end/src/stores/employee.ts
@@ -2,8 +2,11 @@ import type { Employee } from '@/types/Employee'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import employeeService from '@/service/employee'
+import { useLoaderStore } from './loader'
 
 export const useEmployeeStore = defineStore('employee', () => {
+  const loaderStore = useLoaderStore()
+
   const employees = ref(<Employee[]>[])
 
   const initialEmployee: Employee & { files: File[] } = {
@@ -18,8 +21,20 @@ export const useEmployeeStore = defineStore('employee', () => {
     <Employee & { files: File[] }>JSON.parse(JSON.stringify(initialEmployee))
   )
   async function getEmployees() {
+    await loaderStore.doLoad()
+
     const res = await employeeService.getEmployees()
     employees.value = res.data
+    await loaderStore.finishLoad()
+  }
+
+  async function getEmployeesByBranch(employee: Employee) {
+    console.log('test')
+    await loaderStore.doLoad()
+    const res = await employeeService.getEmployeeByBranch(employee)
+    employees.value = res.data
+    console.log(employees)
+    await loaderStore.finishLoad()
   }
 
   async function getEmployee(employee: Employee) {
@@ -51,6 +66,7 @@ export const useEmployeeStore = defineStore('employee', () => {
     clearEditedEmployee,
     addEmployee,
     deleteEmployee,
+    getEmployeesByBranch,
     employees,
     editedEmployee
   }
