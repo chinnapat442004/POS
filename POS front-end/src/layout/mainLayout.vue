2185927d81977590a9308fa1302390b16f1fe4a4
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-05-22 10:52:25 +0700

    Fis navbarComponent and add loader in mainLayout

--- POS front-end/src/layout/mainLayout.vue
+++ POS front-end/src/layout/mainLayout.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+import Loader from '@/components/LoaderComponent.vue'
+</script>
+
 <template>
   <v-layout class="rounded rounded-md">
     <router-view name="menu"> </router-view>
+    <router-view name="header"> </router-view>
     <v-main class="align-center justify-center" style="min-height: 300px">
       <router-view></router-view>
+      <Loader></Loader>
     </v-main>
   </v-layout>
 </template>
