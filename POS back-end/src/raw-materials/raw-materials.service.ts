53e51175b0e473784d4960a5a1fcf47451b9534c
Chinnapat Thongthai <65160157@go.buu.ac.th>
2024-05-30 17:34:36 +0700

    Add findOne, update and remove in rawMaterial service

--- POS back-end/src/raw-materials/raw-materials.service.ts
+++ POS back-end/src/raw-materials/raw-materials.service.ts
@@ -20,14 +20,19 @@ export class RawMaterialsService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} rawMaterial`;
+    return this.rawMaterialsRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateRawMaterialDto: UpdateRawMaterialDto) {
-    return `This action updates a #${id} rawMaterial`;
+  async update(id: number, updateRawMaterialDto: UpdateRawMaterialDto) {
+    const update = await this.rawMaterialsRepository.findOne({ where: { id } });
+    const rawMaterial = { ...update, ...updateRawMaterialDto };
+    return this.rawMaterialsRepository.save(rawMaterial);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rawMaterial`;
+  async remove(id: number) {
+    const rawMaterial = await this.rawMaterialsRepository.findOne({
+      where: { id },
+    });
+    return this.rawMaterialsRepository.remove(rawMaterial);
   }
 }
